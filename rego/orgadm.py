92251ce54cb80484b4ea87d5c36172a65ee94a4e
hbo-renater <herve.bourgault@renater.fr>
2018-10-24 18:17:31 +0200

    added routes to forms

--- rego/orgadm.py
+++ rego/orgadm.py
@@ -1,6 +1,7 @@
 import functools
 
 from flask import render_template, Blueprint
+from rego.forms import EntityForm, OrganizationForm
 
 bp = Blueprint('orgadm', __name__, url_prefix='/orgadm')
 
@@ -8,3 +9,12 @@ bp = Blueprint('orgadm', __name__, url_prefix='/orgadm')
 def index():
     return render_template('orgadm/index.html')
 
+@bp.route('/register_entity', methods=['GET', 'POST'])
+def register_entity():
+    form = EntityForm()
+    return render_template('orgadm/register_entity.html', form=form)
+
+@bp.route('/register_org', methods=['GET', 'POST'])
+def register_org():
+    form = OrganizationForm()
+    return render_template('orgadm/register_org.html', form=form)
