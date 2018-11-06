b07ec54687558ed01c798a142c30e05f0e99b2f3
hbo-renater <herve.bourgault@renater.fr>
2018-11-06 11:33:15 +0100

    forms renamed, custom validators added

--- rego/forms.py
+++ rego/forms.py
@@ -1,17 +1,25 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField
 from wtforms.validators import DataRequired, Email, URL
+from .util.validators import Unique, JsonString
+from .models import Entity, Organization
 
-class OrganizationForm(FlaskForm):
-    org_name = StringField('Organization name', validators=[DataRequired()]) 
+
+class RegisterEntityForm(FlaskForm):
+    entity_id = StringField('Entity ID', validators=[DataRequired(), URL(),
+        Unique(Entity, Entity.entity_id,message='There is already an entity with this entity ID.')]) 
+    entity_jwks = TextAreaField('Entity public key', render_kw={"rows": 10, "cols": 70}, validators=[DataRequired(), JsonString()])
+    entity_statement = TextAreaField('Entity statement', render_kw={"rows": 10, "cols": 70}, validators=[DataRequired(), JsonString()])
+
+
+class RegisterOrganizationForm(FlaskForm):
+    org_name = StringField('Organization name', validators=[DataRequired(),
+        Unique(Organization, Organization.name,message='There is already an organization with this name.')])
     org_legalcontact_name = StringField('Legal contact name', validators=[DataRequired()]) 
     org_legalcontact_email = StringField('Legal contact email', validators=[DataRequired(), Email()])
-    org_techadmin_name = StringField('Technical administrator name', validators=[DataRequired()]) 
-    org_techadmin_email = StringField('Technical administrator email', validators=[DataRequired(), Email()]) 
+    org_techadmin_name = StringField('Tech admin name', validators=[DataRequired()]) 
+    org_techadmin_email = StringField('Tech admin email', validators=[DataRequired(), Email()]) 
    
-class EntityForm(FlaskForm):
-    entity_id = StringField('Entity ID', validators=[DataRequired(), URL()]) 
-    entity_jwks = TextAreaField('Entity public key', validators=[DataRequired()])
-    entity_statement = TextAreaField('Entity statement', validators=[DataRequired()])
+
 
 
